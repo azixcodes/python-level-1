30193b44eba45aae87a212326905f5313c1b6163
azixcodes <aziz@swatitech.com>
2025-07-11 16:59:03 +0500

    filtering data from dictionairy

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-mixed = ["apple",34,"banana",True,40,"cherry"]
-print(mixed[3])
+username = "Aziz"
+users = [
+    {
+        "name": "Aziz",
+        "age": 20,
+        "city": "Faisalabad"
+    },
+    {
+        "name": "Ali",
+        "age": 21,
+        "city": "Lahore"
+    }
+]
+ 
+results = [user for user in users if user["age"]>20]
+print(results)
